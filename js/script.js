0b9ebc1aaf88c2da59343ac438434b444417d439
Manuel <manuelpadilla83@hotmail.com>
2022-01-11 21:07:21 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,120 +13,120 @@ $(window).on("load", function () {
   });
 });
 
-$(document).ready(() => {
-  $("#slides").superslides({
-    animation: "fade",
-    play: 5000,
-    pagination: false
-  });
+// $(document).ready(() => {
+//   $("#slides").superslides({
+//     animation: "fade",
+//     play: 5000,
+//     pagination: false
+//   });
 
-  var typed = new Typed(".typed", {
-    strings: ["Front-End Developer", "Graphic Designer"],
-    typeSpeed: 50,
-    loop: true,
-    startDelay: 500,
-    showCursor: false
-  });
+//   var typed = new Typed(".typed", {
+//     strings: ["Front-End Developer", "Graphic Designer"],
+//     typeSpeed: 50,
+//     loop: true,
+//     startDelay: 500,
+//     showCursor: false
+//   });
 
-  // https://owlcarousel2.github.io/OwlCarousel2/demos/basic.html
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    items: 4,
-    responsive: {
-      0: {
-        items: 1
-      },
-      480: {
-        items: 2
-      },
-      938: {
-        items: 3
-      },
-      938: {
-        items: 4
-      }
-    }
-  });
-
-  // https://github.com/rendro/easy-pie-chart
-
-  // var skillsTopOffset = $(".skillsSection").offset().top;
-  // var statsTopOffset = $(".statsSection").offset().top;
-  // var countUpFinished = false;
-
-  // $(window).scroll(function() {
-  //   if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
-  //     $(".chart").easyPieChart({
-  //       easing: "easeInOut",
-  //       barColor: "#fff",
-  //       trackColor: false,
-  //       scaleColor: false,
-  //       lineWidth: 7,
-  //       size: 152,
-  //       onStep: function(from, to, percent) {
-  //         $(this.el)
-  //           .find(".percent")
-  //           .text(Math.round(percent));
-  //       }
-  //     });
-  //   }
-
-  //   if (
-  //     !countUpFinished &&
-  //     window.pageYOffset > statsTopOffset - $(window).height() + 200
-  //   ) {
-  //     $(".counter").each(function() {
-  //       var element = $(this);
-  //       var endVal = parseInt(element.text());
-
-  //       element.countup(endVal);
-  //     });
-  //     countUpFinished = true;
-  //   }
-  // });
-
-  $("[data-fancybox]").fancybox();
-
-  $("#filters a").click(function () {
-    $("#filters .current").removeClass("current");
-    $(this).addClass("current");
-
-    var selector = $(this).attr("data-filter");
-
-    $(".items").isotope({
-      filter: selector,
-      animationOptions: {
-        duration: 1500,
-        easing: "linear",
-        queue: false
-      }
-    });
-
-    return false;
-  });
-
-  $("#navigation li a").click(function (e) {
-    e.preventDefault();
-
-    var targetElement = $(this).attr("href");
-    var targetPosition = $(targetElement).offset().top;
-    $("html, body").animate({ scrollTop: targetPosition - 100 }, "slow");
-  });
-
-  const nav = $("#navigation");
-  const navTop = nav.offset().top;
-
-  $(window).on("scroll", stickyNavigation);
-
-  function stickyNavigation() {
-    var body = $("body");
-
-    if ($(window).scrollTop() >= navTop) {
-      body.css("padding-top", nav.outerHeight() + "px");
-      body.addClass("fixedNav");
-    } else {
-      body.css("padding-top", 0);
-      body.removeClass("fixedNav");
+// https://owlcarousel2.github.io/OwlCarousel2/demos/basic.html
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  items: 4,
+  responsive: {
+    0: {
+      items: 1
+    },
+    480: {
+      items: 2
+    },
+    938: {
+      items: 3
+    },
+    938: {
+      items: 4
     }
   }
+});
+
+// https://github.com/rendro/easy-pie-chart
+
+// var skillsTopOffset = $(".skillsSection").offset().top;
+// var statsTopOffset = $(".statsSection").offset().top;
+// var countUpFinished = false;
+
+// $(window).scroll(function() {
+//   if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+//     $(".chart").easyPieChart({
+//       easing: "easeInOut",
+//       barColor: "#fff",
+//       trackColor: false,
+//       scaleColor: false,
+//       lineWidth: 7,
+//       size: 152,
+//       onStep: function(from, to, percent) {
+//         $(this.el)
+//           .find(".percent")
+//           .text(Math.round(percent));
+//       }
+//     });
+//   }
+
+//   if (
+//     !countUpFinished &&
+//     window.pageYOffset > statsTopOffset - $(window).height() + 200
+//   ) {
+//     $(".counter").each(function() {
+//       var element = $(this);
+//       var endVal = parseInt(element.text());
+
+//       element.countup(endVal);
+//     });
+//     countUpFinished = true;
+//   }
+// });
+
+// $("[data-fancybox]").fancybox();
+
+// $("#filters a").click(function () {
+//   $("#filters .current").removeClass("current");
+//   $(this).addClass("current");
+
+//   var selector = $(this).attr("data-filter");
+
+//   $(".items").isotope({
+//     filter: selector,
+//     animationOptions: {
+//       duration: 1500,
+//       easing: "linear",
+//       queue: false
+//     }
+//   });
+
+//   return false;
+// });
+
+$("#navigation li a").click(function (e) {
+  e.preventDefault();
+
+  var targetElement = $(this).attr("href");
+  var targetPosition = $(targetElement).offset().top;
+  $("html, body").animate({ scrollTop: targetPosition - 100 }, "slow");
+});
+
+const nav = $("#navigation");
+const navTop = nav.offset().top;
+
+$(window).on("scroll", stickyNavigation);
+
+function stickyNavigation() {
+  var body = $("body");
+
+  if ($(window).scrollTop() >= navTop) {
+    body.css("padding-top", nav.outerHeight() + "px");
+    body.addClass("fixedNav");
+  } else {
+    body.css("padding-top", 0);
+    body.removeClass("fixedNav");
+  }
+}
 });
